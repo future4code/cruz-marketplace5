25b7588812725b3c102554fbbafcc5f81c18a76a
Elber-Sousa <elbe995@gmail.com>
2021-03-30 18:25:03 -0300

    Adicionado a logo e o carrossel de imagens

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,48 +1,57 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
+import ReactDOM from 'react-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import carrossel1 from './img/carrossel-1.png';
+import carrossel2 from './img/carrossel-2.jpg';
+import carrossel3 from './img/carrossel-3.png';
 
 const Principal = styled.div`
-  display:flex;
-  flex-direction:column;
-  height:150vh;
-`
-const Carrossel = styled.div`
-  display:flex;
-  border:1px solid black;
-  flex:1;
-`
+  display: flex;
+  flex-direction: column;
+  height: 150vh;
+`;
 
 const Produtos = styled.div`
-  display:flex;
-  flex:3;
-  border:1px solid red;
-`
+  display: flex;
+  flex: 3;
+  border: 1px solid red;
+`;
+
 export class AppContainer extends React.Component {
-
   state = {
-    showProducts:[]
-  }
+    showProducts: []
+  };
 
-
-  getProducts = async() =>{
-    try{
-        const response = await axios.get("https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products")
-        this.setState({showProducts:response.data.products})
-        console.log("estate",this.state.showProducts);
-    }catch(erro){
-      console.log("erro",erro);
+  getProducts = async () => {
+    try {
+      const response = await axios.get(
+        'https://us-central1-labenu-apis.cloudfunctions.net/fourUsedTwo/products'
+      );
+      this.setState({ showProducts: response.data.products });
+      console.log('estate', this.state.showProducts);
+    } catch (erro) {
+      console.log('erro', erro);
     }
-  }
+  };
 
   render() {
     return (
       <Principal>
-          <Carrossel>
-
-          </Carrossel>
-          <Produtos>
-          </Produtos>
+        <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false} >
+          <div>
+            <img src={carrossel1} />
+          </div>
+          <div>
+            <img src={carrossel2} />
+          </div>
+          <div>
+            <img src={carrossel3} />
+          </div>
+        </Carousel>
+        <Produtos></Produtos>
       </Principal>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,83 +1,93 @@
-import React from 'react'
-import styled from 'styled-components'
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import React from 'react';
+import styled from 'styled-components';
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider
+} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Logo from '../img/Logo.png';
 import TextField from '@material-ui/core/TextField';
-import Badge from "@material-ui/core/Badge";
+import Badge from '@material-ui/core/Badge';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Collapse } from '@material-ui/core';
 
-
 const StyledBadge = withStyles((theme) => ({
-    badge: {
-      right: -3,
-      top: 13,
-      border: `2px solid ${theme.palette.background.paper}`,
-      padding: '0 4px',
-    },
-  }))(Badge);
-  
-  const HeaderContainer = styled.div`
-      display: flex;
-      justify-content: space-around;
-      text-align: center;
-      padding: 0;
-      margin: 0;
-  `
+  badge: {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px'
+  }
+}))(Badge);
 
-  const TextFieldPesquisar = styled(TextField)({
-  })
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+`;
 
-  const BotaoVender = styled(Button)({
-    border: 'none',
-    padding: '8px 10px',
-    margin: '20px 4px',
-    borderRadius: '10px',
-    width: '7%',
-    height: '6%',
-    cursor: 'pointer',
-    '&:hover': {
-      color: 'black',
-      fontSize: '90%',
-      '&:active': {
-        color: 'black',
-      }
+const LogoImg = styled.img`
+  width: 80px;
+  margin: 3px 0;
+  margin-left: -140px;
+`;
+
+const TextFieldPesquisar = styled(TextField)({});
+
+const BotaoVender = styled(Button)({
+  border: 'none',
+  padding: '8px 10px',
+  margin: '20px 4px',
+  borderRadius: '10px',
+  width: '7%',
+  height: '6%',
+  cursor: 'pointer',
+  '&:hover': {
+    color: 'black',
+    fontSize: '90%',
+    '&:active': {
+      color: 'black'
     }
-  });
+  }
+});
 
-  export default class Header extends React.Component {
+export default class Header extends React.Component {
+  state = {};
 
-    state = {
-      
-    }
-    
-        render() {
-            return(
+  render() {
+    return (
+      <HeaderContainer variant="contained" color="primary">
+        <LogoImg src={Logo} />
 
-    <HeaderContainer variant='contained' color="primary">
-      <h1>Logo</h1>
+        <TextFieldPesquisar
+          id="outlined-search"
+          label="Pesquisar"
+          type="search"
+          variant="outlined"
+        />
 
+        <IconButton aria-label="cart">
+          <StyledBadge
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right'
+            }}
+            badgeContent={6}
+            color="secondary"
+          >
+            <ShoppingCartIcon color="primary" />
+          </StyledBadge>
+        </IconButton>
 
-      <TextFieldPesquisar id="outlined-search" label="Pesquisar" type="search" variant="outlined" />
-
-      <IconButton aria-label="cart">
-        <StyledBadge anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-          }} 
-          badgeContent={6} color="secondary">
-          <ShoppingCartIcon color="primary"/>
-        </StyledBadge>
-      </IconButton>
-      
-      <Button variant="outlined" size="small" color="secondary">
-        Vender
-      </Button>
-
-    </HeaderContainer>
-
-    )
+        <Button variant="outlined" size="small" color="secondary">
+          Vender
+        </Button>
+      </HeaderContainer>
+    );
   }
 }
